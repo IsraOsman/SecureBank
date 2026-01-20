fd6288e7177db26fb01662c940315a64532d17a7
israadam1993@gmail.com <israadam1993@gmail.com>
2026-01-19 19:57:55 -0500

    Adding backend app

---
+++ src/main/java/com/isradev/securebank/SecurebankBackendApplication.java
@@ -0,0 +1,14 @@
+package com.isradev.securebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurebankBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurebankBackendApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/isradev/securebank/SecurebankBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isradev.securebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurebankBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
